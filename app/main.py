5f22257a910d9c1022ffb303b32f92518d5b44f8
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-10-19 20:28:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,17 +8,24 @@ URL = "http://api.weatherapi.com/v1/current.json?"
 FILTERING = "Paris"
 API_KEY = os.getenv("API_KEY")
 
+if not API_KEY:
+    raise ValueError(
+        "API_KEY not found! Ensure you have set up "
+        "your API key throw Docker or in .env file."
+    )
+
 
 def get_weather() -> None:
 
     result = requests.get(URL + f"key={API_KEY}&q={FILTERING}")
+    result.raise_for_status()
     data = result.json()
 
-    city = data["location"]["name"]
-    temperature = data["current"]["temp_c"]
-    country = data["location"]["country"]
-    last_update = data["current"]["last_updated"]
-    weather_condition = data["current"]["condition"]["text"]
+    city = data.get("location").get("name")
+    temperature = data.get("current").get("temp_c")
+    country = data.get("location").get("country")
+    last_update = data.get("current").get("last_updated")
+    weather_condition = data.get("current").get("condition").get("text")
 
     print(
         f"Performing request to Weather API for city {city}...\n"
